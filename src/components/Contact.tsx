8441a6343d1ea3b752d670a123f871d3bd3a3488
Tyler Jackson <tylerjackson@Tylers-MacBook-Air.local>
2023-12-29 16:28:37 -0500

    update styling for mobile

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
       <div className="flex items-center flex-wrap justify-center h-full">
         <div className="text-center">
           <h1 className="text-4xl py-2">For any questions please reach out</h1>
-          <h1 className="text-4xl py-2 bg-gradient-to-r from-indigo-500 via-slate-500 to-cyan-600 inline-block text-transparent bg-clip-text">
+          <h1 className="text-3xl md:text-4xl py-2 bg-gradient-to-r from-indigo-500 via-slate-500 to-cyan-600 inline-block text-transparent bg-clip-text">
             99jacksont@gmail.com
           </h1>
         </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <div className="flex justify-center p-3">
+    <div className="flex justify-center p-5">
       <h1>&copy; Tyler Jackson {year}</h1>
     </div>
   );
